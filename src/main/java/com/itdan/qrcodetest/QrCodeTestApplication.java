165658180db96b9c384d57001afe1207e7745cd7
18376108492 <2207161187@qq.com>
2020-05-28 23:10:51 +0800

    Initial commit

---
+++ src/main/java/com/itdan/qrcodetest/QrCodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.itdan.qrcodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itdan/qrcodetest/QrCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itdan.qrcodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
